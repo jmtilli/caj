3059aa45a59b68f7cb8a7db9aff5bd139f260a78
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2026-03-11 20:15:41 +0200

    Fix containerof to be standards compliant

--- cajun/cajcontainerof.h
+++ cajun/cajcontainerof.h
@@ -1,7 +1,9 @@
 #ifndef _CAJ_CONTAINEROF_H_
 #define _CAJ_CONTAINEROF_H_
 
+#include <stddef.h>
+
 #define CAJ_CONTAINER_OF(ptr, type, member) \
-  ((type*)(((char*)ptr) - (((char*)&(((type*)0)->member)) - ((char*)0))))
+  ((type*)(((char*)ptr) - offsetof(type, member)))
 
 #endif
